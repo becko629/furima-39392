b3da9e9ddd298f5f3dea96583d335c1421e9537a
becko629 <beck16575479@gmail.com>
2023-09-12 10:06:14 +0900

    商品情報編集機能 #8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,11 +24,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    return unless @item.user_id != current_user.id
+
+    redirect_to root_path
   end
 
   def update
-    @item =Item.find(params[:id])
-    @item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
